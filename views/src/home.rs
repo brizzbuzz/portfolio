21e75b997308a0922f8888b94beb7f181f16f64a
Ryan Brink <dev@ryanbr.ink>
2024-03-02 09:55:26 -0500

    misc

--- views/src/home.rs
+++ views/src/home.rs
@@ -3,11 +3,15 @@ use rocket::get;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-pub struct HelloTemplate<'a> {
+pub struct HomeTemplate<'a> {
     pub name: &'a str,
+    pub description: &'a str,
 }
 
 #[get("/")]
-pub fn index() -> HelloTemplate<'static> {
-    HelloTemplate { name: "Ryan Brink" }
+pub fn index() -> HomeTemplate<'static> {
+    HomeTemplate {
+        name: "RYAN BRINK",
+        description: "I’m Ryan, a software developer and all-around technochad based in New York City. Occasionally I write things, more often I code things. AI art enthusiast."
+    }
 }
