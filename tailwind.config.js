a722d9ed22d8fe7a339d189ea86450047aadcf91
Ryan Brink <5607577+brizzbuzz@users.noreply.github.com>
2024-03-05 09:19:10 -0500

    feat: basic home page (#2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["views/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      blur: {
+        xxs: '1px',
+        xs: '2px',
+      },
+      colors: {
+        matrix: {
+          50: "#8AEBAD",
+          100: "#22C55E",
+          200: "#1EAE53",
+          300: "#1A9848",
+          400: "#16833E",
+          500: "#136D34",
+          600: "#0F5729",
+          700: "#0B411F",
+          800: "#072C15",
+          900: "#04160A",
+          950: "#020D06"
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
--- views/src/home.rs
+++ views/src/home.rs
@@ -1,13 +1,45 @@
 use askama::Template;
 use rocket::get;
 
+pub struct Profile<'a> {
+    image_src: &'a str,
+}
+
+pub struct Character<'a> {
+    image_src: &'a str,
+    rotation: &'a str,
+}
+
 #[derive(Template)]
-#[template(path = "index.html")]
-pub struct HelloTemplate<'a> {
+#[template(path = "home.html")]
+pub struct HomeTemplate<'a> {
     pub name: &'a str,
+    pub description: &'a str,
+    pub profiles: Vec<Profile<'a>>,
+    pub characters: Vec<Character<'a>>,
 }
 
 #[get("/")]
-pub fn index() -> HelloTemplate<'static> {
-    HelloTemplate { name: "Ryan" }
+pub fn index() -> HomeTemplate<'static> {
+    let profiles = vec![
+        Profile { image_src: "/public/images/logos/github.png" },
+        Profile { image_src: "/public/images/logos/instagram.png" },
+        Profile { image_src: "/public/images/logos/linkedin.png" },
+        Profile { image_src: "/public/images/logos/x.png" },
+    ];
+
+    let characters = vec![
+        Character { image_src: "/public/images/characters/cyberpunk/distressed-profess.png", rotation: "rotate-2" },
+        Character { image_src: "/public/images/characters/cyberpunk/pirate-squirrel.png", rotation: "-rotate-2" },
+        Character { image_src: "/public/images/characters/cyberpunk/the-man-who-snapped.png", rotation: "rotate-2" },
+        Character { image_src: "/public/images/characters/cyberpunk/young-princess.png", rotation: "rotate-2" },
+        Character { image_src: "/public/images/characters/cyberpunk/evil-samurai.png", rotation: "-rotate-2" },
+    ];
+
+    HomeTemplate {
+        name: "RYAN BRINK",
+        description: "Cyberspace Cowboy :: Occasionally I write things, more often I code things",
+        profiles,
+        characters,
+    }
 }
